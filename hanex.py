7ae8b715416ede54403d8088f8bcd39c7ef602c6
KxllSwxtch <dimashin9@gmail.com>
2024-11-08 15:02:28 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -293,7 +293,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left с явным ожиданием
         try:
-            product_left = WebDriverWait(driver, 15).until(
+            product_left = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "product_left"))
             )
             product_left_splitted = product_left.text.split("\n")
