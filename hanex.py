8ba177a7ce29d086b05e272ec8cd5d205079b322
KxllSwxtch <dimashin9@gmail.com>
2024-11-13 13:43:39 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -294,7 +294,7 @@ def get_car_info(url):
 
         # Проверка элемента areaLeaseRent
         try:
-            lease_area = WebDriverWait(driver, 3).until(
+            lease_area = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.ID, "areaLeaseRent"))
             )
             title_element = lease_area.find_element(By.CLASS_NAME, "title")
@@ -315,7 +315,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left
         try:
-            product_left = WebDriverWait(driver, 6).until(
+            product_left = WebDriverWait(driver, 7).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "product_left"))
             )
             product_left_splitted = product_left.text.split("\n")
