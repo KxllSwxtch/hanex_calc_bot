be203733128e9fabcc00ca196c7e527ef9770066
KxllSwxtch <dimashin9@gmail.com>
2024-11-18 22:19:36 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -274,6 +274,7 @@ def get_car_info(url):
         driver.get(url)
         check_and_handle_alert(driver)
         load_cookies(driver)
+        driver.refresh()
 
         # Проверка на reCAPTCHA
         # if "reCAPTCHA" in driver.page_source:
