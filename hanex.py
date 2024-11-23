a64f8457d8264c796a73ab83f58997e69e0bc01c
KxllSwxtch <dimashin9@gmail.com>
2024-11-23 12:02:10 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -281,12 +281,11 @@ def check_and_handle_alert(driver):
 def get_car_info(url):
     global car_id_external
 
-    chrome_options = uc.ChromeOptions(headless=True, use_subprocess=False)
+    chrome_options = uc.ChromeOptions()
     chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")  # Необходим для работы в Heroku
     chrome_options.add_argument("--disable-dev-shm-usage")  # Решает проблемы с памятью
-    # chrome_options.add_argument("--window-size=1920,1080")  # Устанавливает размер окна
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
@@ -295,7 +294,7 @@ def get_car_info(url):
     # Инициализация драйвера
     service = Service(CHROMEDRIVER_PATH)
     # driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver = uc.Chrome(options=chrome_options)
+    driver = uc.Chrome(headless=True, use_subprocess=False, options=chrome_options)
 
     try:
         # Загружаем страницу
