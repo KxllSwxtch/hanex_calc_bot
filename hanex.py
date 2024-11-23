b4d0c513ff804b536e57f6f9336341fffbcbeafd
KxllSwxtch <dimashin9@gmail.com>
2024-11-23 19:52:00 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -301,7 +301,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left
         try:
-            product_left = driver.find_element(By.CSS_SELECTOR, "div.product_left")
+            product_left = driver.find_element(By.CLASS_NAME, "product_left")
             product_left_splitted = product_left.text.split("\n")
 
             car_title = product_left.find_element(
