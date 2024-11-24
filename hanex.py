bba59d4fd07d450496e819669ca0ca8c098a5dd6
KxllSwxtch <dimashin9@gmail.com>
2024-11-24 13:10:49 +0900

    updates

--- hanex.py
+++ hanex.py
@@ -322,6 +322,7 @@ def get_car_info(url):
         if "reCAPTCHA" in driver.page_source:
             print("Обнаружена reCAPTCHA. Пытаемся решить...")
             solve_recaptcha(driver, SITE_KEY, url)
+            time.sleep(5)
 
         wait_for_page_to_load(driver)
 
